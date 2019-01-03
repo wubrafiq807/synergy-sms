d94cd1d0df319b06b0fdc10095a62f0a1dd4b53c
mehedi.sahan <mahadi.hasan@dynamicsrv.com>
2019-01-03 17:55:31 +0600

    no message

---
+++ src/main/java/com/nazdaq/sms/beans/SmsAdvanceBean.java
@@ -0,0 +1,114 @@
+package com.nazdaq.sms.beans;
+
+public class SmsAdvanceBean {
+	private String reqNumber;
+	private String submissionDate;
+	
+	private String employeeName;
+	private String employeeId;
+	private String employeeDesignation;
+	
+	private String approvedAmount;
+	private String amountInWord;
+	private String reqPurpose;
+	private String stagename;
+	private String status;
+	private String buttontext;
+	private String acttionBy;
+	private String actionedDateText;
+	private String actionedDate;
+	private String comments;
+	
+	
+	public String getReqNumber() {
+		return reqNumber;
+	}
+	public void setReqNumber(String reqNumber) {
+		this.reqNumber = reqNumber;
+	}
+	public String getSubmissionDate() {
+		return submissionDate;
+	}
+	public void setSubmissionDate(String submissionDate) {
+		this.submissionDate = submissionDate;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeDesignation() {
+		return employeeDesignation;
+	}
+	public void setEmployeeDesignation(String employeeDesignation) {
+		this.employeeDesignation = employeeDesignation;
+	}
+	public String getApprovedAmount() {
+		return approvedAmount;
+	}
+	public void setApprovedAmount(String approvedAmount) {
+		this.approvedAmount = approvedAmount;
+	}
+	public String getAmountInWord() {
+		return amountInWord;
+	}
+	public void setAmountInWord(String amountInWord) {
+		this.amountInWord = amountInWord;
+	}
+	public String getReqPurpose() {
+		return reqPurpose;
+	}
+	public void setReqPurpose(String reqPurpose) {
+		this.reqPurpose = reqPurpose;
+	}
+	public String getStagename() {
+		return stagename;
+	}
+	public void setStagename(String stagename) {
+		this.stagename = stagename;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getButtontext() {
+		return buttontext;
+	}
+	public void setButtontext(String buttontext) {
+		this.buttontext = buttontext;
+	}
+	public String getActtionBy() {
+		return acttionBy;
+	}
+	public void setActtionBy(String acttionBy) {
+		this.acttionBy = acttionBy;
+	}
+	public String getActionedDateText() {
+		return actionedDateText;
+	}
+	public void setActionedDateText(String actionedDateText) {
+		this.actionedDateText = actionedDateText;
+	}
+	public String getActionedDate() {
+		return actionedDate;
+	}
+	public void setActionedDate(String actionedDate) {
+		this.actionedDate = actionedDate;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+}
--- src/main/java/com/nazdaq/sms/controller/ReportController.java
+++ src/main/java/com/nazdaq/sms/controller/ReportController.java
@@ -1,15 +1,48 @@
 package com.nazdaq.sms.controller;
 
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.nazdaq.sms.beans.SmsAdvanceBean;
+import com.nazdaq.sms.model.ProductPriceHistory;
+import com.nazdaq.sms.model.Requisition;
+import com.nazdaq.sms.model.RequisitionHistory;
+import com.nazdaq.sms.model.RequisitionItem;
 import com.nazdaq.sms.service.CommonService;
 import com.nazdaq.sms.util.Constants;
+import com.nazdaq.sms.util.EnglishNumberToWords;
+import com.nazdaq.sms.util.NumberWordConverter;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Controller
 @PropertySource("classpath:common.properties")
@@ -24,5 +57,129 @@ public class ReportController implements Constants {
 	@Value("${cc.email.addresss}")
 	String ccEmailAddresss;
 
+	// methods to company show
+	@RequestMapping(value = "/jobAdvanceReport/{id}", method = RequestMethod.GET)
+	public void jobAdvanceReport(@PathVariable("id") String id, ModelMap model, RedirectAttributes redirectAttributes,
+			Principal principal, HttpSession session, HttpServletResponse response)
+			throws JRException, IOException, ParseException {
+
+		JRDataSource jRdataSource = null;
+
+		Requisition requisition = (Requisition) commonService.getAnObjectByAnyUniqueColumn("Requisition", "id", id);
+
+		String hqlQuery = "From RequisitionHistory where requisition_id = " + requisition.getId() + " order by id asc";
+		List<RequisitionHistory> smsRequisitionHistoryList = commonService.getObjectListByHqlQuery(hqlQuery).stream()
+				.map(e -> (RequisitionHistory) e).filter(x -> x.getIsRejected() != 1).collect(Collectors.toList());
+
+		List<SmsAdvanceBean> smsAdvanceBeans = new ArrayList<>();
+
+		String hqlQueryForRequistionItem = "From RequisitionItem where requisition_id = " + requisition.getId()
+				+ " order by id asc";
+
+		List<RequisitionItem> requisitionItems = commonService.getObjectListByHqlQuery(hqlQueryForRequistionItem)
+				.stream().map(x -> (RequisitionItem) x).collect(Collectors.toList());
+
+		Double totalApproveAmount = 0.0;
+
+		for (RequisitionItem requisitionItem : requisitionItems) {
+
+			Integer pId = requisitionItem.getProduct().getId();
+
+			Double wAvg = getWeighttedAvgPrice(pId);
+
+			totalApproveAmount += wAvg * requisitionItem.getQuantity();
+
+		}
+
+		for (RequisitionHistory requisitionHistory : smsRequisitionHistoryList) {
+			if (requisitionHistory.getSettings() != null) {
+				SmsAdvanceBean smsAdvanceBean = new SmsAdvanceBean();
+
+				smsAdvanceBean.setEmployeeDesignation(requisition.getEmployee().getDesignation());
+
+				smsAdvanceBean.setEmployeeId(requisition.getEmployee().getLxnId());
+				smsAdvanceBean.setEmployeeName(requisition.getEmployee().getName());
+
+				smsAdvanceBean.setApprovedAmount(NumberWordConverter.convertDoubleToCurrency(totalApproveAmount));
+
+				smsAdvanceBean.setAmountInWord(EnglishNumberToWords.convert(Math.round(totalApproveAmount)));
+				smsAdvanceBean.setReqPurpose(requisition.getPurpose());
+				smsAdvanceBean.setReqNumber("REQ:" + requisition.getEmployee().getId());
+
+				smsAdvanceBean.setSubmissionDate(
+						NumberWordConverter.getCustomDateFromDateFormate(requisition.getCreatedDate().toString()));
+
+				smsAdvanceBean.setStagename(this.getamounText(requisitionHistory) + " by" + " "
+						+ requisitionHistory.getCreatedBy().getName() + "("
+						+ requisitionHistory.getCreatedBy().getDesignation() + ")");
+
+				smsAdvanceBean.setStatus("Status : " + this.getamounText(requisitionHistory));
+
+				smsAdvanceBean.setButtontext(requisition.getSettings().getBtnText());
+				smsAdvanceBean.setActtionBy(requisitionHistory.getCreatedBy().getName());
+
+				smsAdvanceBean.setActionedDateText(this.getamounText(requisitionHistory) + " Date");
+
+				smsAdvanceBean.setActionedDate(NumberWordConverter
+						.getCustomDateFromDateFormate(requisitionHistory.getCreatedDate().toString()));
+
+				smsAdvanceBean.setComments(requisitionHistory.getRemarks());
+
+				smsAdvanceBeans.add(smsAdvanceBean);
+
+			}
+
+			// @formatter:on
+
+			InputStream jasperStream = null;
+
+			jasperStream = this.getClass().getResourceAsStream("/report/smsReportForm.jasper");
+			SmsAdvanceBean smsAdvanceBean = new SmsAdvanceBean();
+			Map<String, Object> params = new HashMap<>();
+			Map<String, Object> datasourcemap = new HashMap<>();
+			datasourcemap.put("SmsAdvanceBean", smsAdvanceBean);
+			jRdataSource = new JRBeanCollectionDataSource(smsAdvanceBeans, false);
+
+			params.put("datasource", jRdataSource);
+
+			// prepare report first for one
+			JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, jRdataSource);
+
+			response.setContentType("application/x-pdf");
+			response.setHeader("Content-disposition",
+					"inline; filename=job_advance_" + requisition.getId() + "_"
+							+ requisition.getEmployee().getName().trim().replaceAll("\\.", "_").replace(" ", "_") + ""
+							+ ".pdf");
+			final OutputStream outStream = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+		}
+	}
+
+	private String getamounText(RequisitionHistory requisitionHistory) {
+		String text = "";
+		if (requisitionHistory.getSettings().getBtnText().equals("Approve")) {
+			text = "d";
+		} else if (requisitionHistory.getSettings().getBtnText().equals("Submit")) {
+			text = "ted";
+		} else {
+			text = "ed";
+		}
+		return new String((requisitionHistory.getSettings().getBtnText() + text));
+	}
+
+	private Double getWeighttedAvgPrice(Integer productID) {
+		List<ProductPriceHistory> productPriceHistories = commonService
+				.getObjectListByAnyColumn("ProductPriceHistory", "product_id", productID.toString()).stream()
+				.map(x -> (ProductPriceHistory) x).collect(Collectors.toList());
+		Double sum = 0.0;
+		int sumQuantity = 0;
+		for (ProductPriceHistory productPriceHistory : productPriceHistories) {
+			sum += productPriceHistory.getPrice() * productPriceHistory.getPurchaseQuantity();
+			sumQuantity += productPriceHistory.getPurchaseQuantity();
+		}
+
+		return sum / sumQuantity;
+	}
 
 }
